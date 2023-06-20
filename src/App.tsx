962a15c4f30d826eecb8ce6e690c6480570c8cff
Sultan <sitexpressorg@gmail.com>
2023-06-20 18:28:52 +0700

    Commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Hello Git</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link-2"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Learn React
         </a>
       </header>
     </div>
